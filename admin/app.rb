59b3f7cd4c137a66fc0cfedbec2952bc8a62a126
Lital Zubery <lital.z@fiverr.com>
2014-10-21 23:08:29 +0300

    [test]

--- admin/app.rb
+++ admin/app.rb
@@ -27,7 +27,7 @@ module D2d
 
     set :delivery_method, :smtp => {
       :address              => 'smtp-out-pool.greenpeace.org',
-      :port                 => 587,
+      :port                 => 25,
       #:user_name            => '###',
       #:password             => '###',
       #:authentication       => :plain,
